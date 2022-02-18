fccf09926f93707f59cdac301a3b94875d4150bb
ITlqad1699 <lqad1649engineer@gmail.com>
2022-02-18 17:07:04 +0700

    update

---
+++ src/ss3_array_and_method_on_java/bai_tap/CountingOccurencesOfCharacter.java
@@ -0,0 +1,23 @@
+package ss3_array_and_method_on_java.bai_tap;
+
+import java.util.Scanner;
+
+public class CountingOccurencesOfCharacter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("<----------------------------------------Intialize A String And Character-------------------------------------------------->\n");
+        System.out.print("Enter a string: ");
+        String str = scanner.nextLine();
+        System.out.print("Enter a character: ");
+        //syntax of declaration character:
+        char charactor = scanner.next().charAt(0);
+        System.out.print("<----------------------------------------Count Occurrences Of Character-------------------------------------------------->\n");
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (charactor == str.charAt(i)) {
+                count += 1;
+            }
+        }
+        System.out.print("the character " + charactor + " is apear " + count + " times");
+    }
+}
